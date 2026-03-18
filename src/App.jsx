b5d936cfe42cffdcb0754ac8d4fb0641ee987fc3
Mohamed Tariket <tariketmohameduniv@gmail.com>
2026-03-18 17:29:57 +0100

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import PostsPage from './pages/PostsPage'
+import PostDetails from './pages/PostDetails'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
